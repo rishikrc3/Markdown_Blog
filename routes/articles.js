93df91a62b9f2620dc20a9f4a63a4fe0b6e18b1e
Rishik Chakravarty <rishikrc3@gmail.com>
2023-06-19 13:11:32 +0530

    Creating the new article form

--- routes/articles.js
+++ routes/articles.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Inside the articles");
+  res.send("h1");
 });
 
+router.get("/new", (req, res) => {
+  res.render("articles/new");
+});
+
+router.post("/", (res, req) => {});
 module.exports = router;
